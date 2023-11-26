3ea774ebb648ee5acaedf27928e60dffdd9178a2
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-11-25 17:06:16 -0700

    docs: update basic example to encounter rendertarget bug based off @musjj's code

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,16 +1,69 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    render::{
+        camera::RenderTarget,
+        render_resource::{
+            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+        },
+        texture::BevyDefault,
+        view::RenderLayers,
+    },
+};
 use bevy_ecs_tilemap::prelude::*;
 
 mod helpers;
 
+#[derive(Component)]
+struct CanvasCamera;
+#[derive(Component)]
+struct Canvas;
+const WIDTH: f32 = 256.;
+const HEIGHT: f32 = 256.;
+
 fn startup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
+    mut images: ResMut<Assets<Image>>,
     #[cfg(all(not(feature = "atlas"), feature = "render"))] array_texture_loader: Res<
         ArrayTextureLoader,
     >,
 ) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((Camera2dBundle::default(), RenderLayers::layer(1)));
+
+    let size = Extent3d {
+        width: WIDTH as u32,
+        height: HEIGHT as u32,
+        ..default()
+    };
+
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::bevy_default(),
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
+
+    image.resize(size);
+    let canvas = images.add(image);
+
+    commands.spawn((
+        SpriteBundle {
+            texture: canvas.clone(),
+            transform: Transform::from_scale(Vec3::splat(2.)),
+            ..default()
+        },
+        Canvas,
+        RenderLayers::layer(1),
+    ));
 
     let texture_handle: Handle<Image> = asset_server.load("tiles.png");
 
@@ -113,5 +166,33 @@ fn main() {
         .add_systems(Startup, startup)
         .add_systems(Update, helpers::camera::movement)
         .add_systems(Update, swap_texture_or_hide)
+        .add_systems(Update, add_camera)
         .run();
+}
+
+fn add_camera(
+    camera: Query<&CanvasCamera>,
+    mut commands: Commands,
+    mut frame_count: Local<usize>,
+    canvas_query: Query<&Handle<Image>, With<Canvas>>,
+) {
+    if *frame_count > 10 {
+        if camera.is_empty() {
+            let canvas = canvas_query.single();
+            println!("spawning rendertarget camera");
+            commands.spawn((
+                Camera2dBundle {
+                    camera: Camera {
+                        order: -1,
+                        target: RenderTarget::Image(canvas.clone()),
+                        ..default()
+                    },
+                    ..default()
+                },
+                CanvasCamera,
+            ));
+        }
+    } else {
+        *frame_count += 1;
+    }
 }
